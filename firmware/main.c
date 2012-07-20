caf6cc8e017a8aa5b9d1bcc24180c7a56332edf9
Shu Ning Bian <freespace@gmail.com>
2012-07-20 21:32:04 +1000

    Indent improvements

--- firmware/main.c
+++ firmware/main.c
@@ -23,23 +23,22 @@ at least be connected to INT0 as well.
 
 #include <avr/pgmspace.h>   /* required by usbdrv.h */
 #include "usbdrv.h"
-#include "oddebug.h"        /* This is also an example for using debug macros */
 
 /* ------------------------------------------------------------------------- */
 /* ----------------------------- USB interface ----------------------------- */
 /* ------------------------------------------------------------------------- */
 
 PROGMEM char const usbHidReportDescriptor[22] = {    /* USB report descriptor */
-    0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
-    0x09, 0x01,                    // USAGE (Vendor Usage 1)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x80,                    //   REPORT_COUNT (128)
-    0x09, 0x00,                    //   USAGE (Undefined)
-    0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
-    0xc0                           // END_COLLECTION
+  0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
+  0x09, 0x01,                    // USAGE (Vendor Usage 1)
+  0xa1, 0x01,                    // COLLECTION (Application)
+  0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+  0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
+  0x75, 0x08,                    //   REPORT_SIZE (8)
+  0x95, 0x80,                    //   REPORT_COUNT (128)
+  0x09, 0x00,                    //   USAGE (Undefined)
+  0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
+  0xc0                           // END_COLLECTION
 };
 /* Since we define only one feature report, we don't use report-IDs (which
  * would be the first byte of the report). The entire report consists of 128
@@ -47,95 +46,90 @@ PROGMEM char const usbHidReportDescriptor[22] = {    /* USB report descriptor */
  */
 
 /* The following variables store the status of the current data transfer */
-static uchar    currentAddress;
-static uchar    bytesRemaining;
+static uchar  currentAddress;
+static uchar  bytesRemaining;
 
 /* ------------------------------------------------------------------------- */
 
 /* usbFunctionRead() is called when the host requests a chunk of data from
  * the device. For more information see the documentation in usbdrv/usbdrv.h.
  */
-uchar   usbFunctionRead(uchar *data, uchar len)
-{
-    if(len > bytesRemaining)
-        len = bytesRemaining;
-    eeprom_read_block(data, (uchar *)0 + currentAddress, len);
-    currentAddress += len;
-    bytesRemaining -= len;
-    return len;
+uchar usbFunctionRead(uchar *data, uchar len) {
+  if(len > bytesRemaining) len = bytesRemaining;
+
+  eeprom_read_block(data, (uchar *)0 + currentAddress, len);
+  currentAddress += len;
+  bytesRemaining -= len;
+
+  return len;
 }
 
 /* usbFunctionWrite() is called when the host sends a chunk of data to the
  * device. For more information see the documentation in usbdrv/usbdrv.h.
  */
-uchar   usbFunctionWrite(uchar *data, uchar len)
-{
-    if(bytesRemaining == 0)
-        return 1;               /* end of transfer */
-    if(len > bytesRemaining)
-        len = bytesRemaining;
-    eeprom_write_block(data, (uchar *)0 + currentAddress, len);
-    currentAddress += len;
-    bytesRemaining -= len;
-    return bytesRemaining == 0; /* return 1 if this was the last chunk */
+uchar usbFunctionWrite(uchar *data, uchar len) {
+  if(bytesRemaining == 0) return 1;
+
+  if(len > bytesRemaining)
+    len = bytesRemaining;
+
+  eeprom_write_block(data, (uchar *)0 + currentAddress, len);
+  currentAddress += len;
+  bytesRemaining -= len;
+  return bytesRemaining == 0; /* return 1 if this was the last chunk */
 }
 
 /* ------------------------------------------------------------------------- */
 
-usbMsgLen_t usbFunctionSetup(uchar data[8])
-{
-usbRequest_t    *rq = (void *)data;
+usbMsgLen_t usbFunctionSetup(uchar data[8]) {
+  usbRequest_t *rq = (void *)data;
 
-    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* HID class request */
-        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
-            /* since we have only one report type, we can ignore the report-ID */
-            bytesRemaining = 128;
-            currentAddress = 0;
-            return USB_NO_MSG;  /* use usbFunctionRead() to obtain data */
-        }else if(rq->bRequest == USBRQ_HID_SET_REPORT){
-            /* since we have only one report type, we can ignore the report-ID */
-            bytesRemaining = 128;
-            currentAddress = 0;
-            return USB_NO_MSG;  /* use usbFunctionWrite() to receive data from host */
-        }
-    }else{
-        /* ignore vendor type requests, we don't use any */
+  if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {  /* HID class request */
+    if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+      /* since we have only one report type, we can ignore the report-ID */
+      bytesRemaining = 128;
+      currentAddress = 0;
+      return USB_NO_MSG;  /* use usbFunctionRead() to obtain data */
+    }else if(rq->bRequest == USBRQ_HID_SET_REPORT){
+      /* since we have only one report type, we can ignore the report-ID */
+      bytesRemaining = 128;
+      currentAddress = 0;
+      return USB_NO_MSG;  /* use usbFunctionWrite() to receive data from host */
     }
-    return 0;
+  } else {
+    /* ignore vendor type requests, we don't use any */
+  }
+  return 0;
 }
 
 /* ------------------------------------------------------------------------- */
 
-int main(void)
-{
-uchar   i;
+int main(void) {
+  uchar   i = 0;
 
-    wdt_enable(WDTO_1S);
-    /* Even if you don't use the watchdog, turn it off here. On newer devices,
-     * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
-     */
-    /* RESET status: all port bits are inputs without pull-up.
-     * That's the way we need D+ and D-. Therefore we don't need any
-     * additional hardware initialization.
-     */
-    odDebugInit();
-    DBG1(0x00, 0, 0);       /* debug output: main starts */
-    usbInit();
-    usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
-    i = 0;
-    while(--i){             /* fake USB disconnect for > 250 ms */
-        wdt_reset();
-        _delay_ms(1);
-    }
-    usbDeviceConnect();
-    sei();
-    DBG1(0x01, 0, 0);       /* debug output: main loop starts */
-    for(;;){                /* main event loop */
-        DBG1(0x02, 0, 0);   /* debug output: main loop iterates */
-        wdt_reset();
-        usbPoll();
-    }
-    return 0;
+  wdt_enable(WDTO_1S);
+  /* Even if you don't use the watchdog, turn it off here. On newer devices,
+   * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
+   */
+  /* RESET status: all port bits are inputs without pull-up.
+   * That's the way we need D+ and D-. Therefore we don't need any
+   * additional hardware initialization.
+   */
+  usbInit();
+  usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
+
+  while(--i) {       /* fake USB disconnect for > 250 ms */
+    wdt_reset();
+    _delay_ms(1);
+  }
+  usbDeviceConnect();
+
+  sei();
+  for(;;) {        /* main event loop */
+    wdt_reset();
+    usbPoll();
+  }
+  return 0;
 }
 
 /* ------------------------------------------------------------------------- */
@@ -143,31 +137,32 @@ uchar   i;
 
 // Called by V-USB after device reset
 void hadUsbReset() {
-    int frameLength, targetLength = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
-    int bestDeviation = 9999;
-    uchar trialCal, bestCal, step, region;
+  int frameLength, targetLength = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
+  int bestDeviation = 9999;
+  uchar trialCal, bestCal, step, region, hasBestCal;
 
-    // do a binary search in regions 0-127 and 128-255 to get optimum OSCCAL
-    for(region = 0; region <= 1; region++) {
-        frameLength = 0;
-        trialCal = (region == 0) ? 0 : 128;
-        
-        for(step = 64; step > 0; step >>= 1) { 
-            if(frameLength < targetLength) // true for initial iteration
-                trialCal += step; // frequency too low
-            else
-                trialCal -= step; // frequency too high
-                
-            OSCCAL = trialCal;
-            frameLength = usbMeasureFrameLength();
-            
-            if(abs(frameLength-targetLength) < bestDeviation) {
-                bestCal = trialCal; // new optimum found
-                bestDeviation = abs(frameLength -targetLength);
-            }
-        }
+
+  // do a binary search in regions 0-127 and 128-255 to get optimum OSCCAL
+  for(region = hasBestCal = 0; region <= 1; region++) {
+    frameLength = 0;
+    trialCal = (region == 0) ? 0 : 128;
+
+    for(step = 64; step > 0; step >>= 1) {
+      if(frameLength < targetLength) // true for initial iteration
+        trialCal += step; // frequency too low
+      else
+        trialCal -= step; // frequency too high
+
+      OSCCAL = trialCal;
+      frameLength = usbMeasureFrameLength();
+
+      if(abs(frameLength-targetLength) < bestDeviation) {
+        bestCal = trialCal; // new optimum found
+        bestDeviation = abs(frameLength -targetLength);
+      }
     }
+  }
 
-    OSCCAL = bestCal;
+  if (hasBestCal) OSCCAL = bestCal;
 }
 #undef abs
