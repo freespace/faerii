9309d699cc435732755a390ce9206bda42f759e2
Shu Ning Bian <freespace@gmail.com>
2012-07-22 01:08:40 +1000

    Working ctrBlock implementation. Checks for 0xfa 0xe2 0x11 0xee in setup

--- firmware/ctrBlock.c
+++ firmware/ctrBlock.c
@@ -5,17 +5,29 @@
 uint16 _eepromAddr;
 ControlBlock _curBlock;
 
+#define INC_EEPROM_ADDR()   (_eepromAddr += sizeof(_curBlock))
+#define DEC_EEPROM_ADDR()   (_eepromAddr -= sizeof(_curBlock))
+
 void readCtrBlock() {
   eeprom_read_block(&_curBlock, (const void*)_eepromAddr, sizeof(_curBlock));
 }
 
+/**
+ * Rewinds _eepromAddr to the first block after setup block, or the
+ * the first block after next lowest delimiter block.
+ *
+ * After calling this function, _curBlock contains valid control
+ * values, and _eepromAddr points to a valid block.
+ * */
 void rewind() {
-  // rewinds to either the last delimiter block, or to 0
   while (_eepromAddr>0) {
-    _eepromAddr -= sizeof(_curBlock);
+    DEC_EEPROM_ADDR();
     readCtrBlock();
     if (_curBlock.duration == 0xff) break;
   }
+
+  INC_EEPROM_ADDR();
+  readCtrBlock();
 }
 
 ControlBlock *ctrBlockCurrent() { return &_curBlock;}
@@ -23,23 +35,30 @@ ControlBlock *ctrBlockCurrent() { return &_curBlock;}
 ControlBlock *ctrBlockSetup() {
   readCtrBlock();
 
-  // $todo parse options here
-  return &_curBlock;
+  // check to see if the EEPROM has been initalised
+  if (  _curBlock.r   != 0xfa ||
+        _curBlock.g != 0xe2 ||
+        _curBlock.b  != 0x11 ||
+        _curBlock.duration != 0xee ) return NULL;
+  else {
+    // read the next block which actually contains
+    // control information
+    ctrBlockNext();
+
+    // $todo parse options here
+    return &_curBlock;
+  }
 }
 
 ControlBlock *ctrBlockNext() {
   // $todo implement reversal here
-  _eepromAddr += sizeof(_curBlock);
+  INC_EEPROM_ADDR();
 
   if (_eepromAddr >= 512) rewind();
 
   readCtrBlock();
 
-  if (_curBlock.duration == 0xff) {
-    rewind();
-    // need this b/c rewind set curBlock to a delimiter block
-    readCtrBlock();
-  }
+  if (_curBlock.duration == 0xff) rewind();
 
   return &_curBlock;
 }
--- firmware/ctrBlock.h
+++ firmware/ctrBlock.h
@@ -1,5 +1,8 @@
 #include "types.h"
 
+#ifndef _CTRBLOCK_H
+#define _CTRBLOCK_H
 ControlBlock *ctrBlockSetup();
 ControlBlock *ctrBlockCurrent();
 ControlBlock *ctrBlockNext();
+#endif
