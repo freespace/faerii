66e7c4d35091ed0e9bc9825f80af02860badb1cb
Shu Ning Bian <freespace@gmail.com>
2012-07-22 15:04:22 +1000

    Adds ctrBlockGoto and uses config.h for EEPROM_SIZE

--- firmware/ctrBlock.c
+++ firmware/ctrBlock.c
@@ -1,15 +1,18 @@
 #include <avr/eeprom.h>
 
+#include "config.h"
 #include "ctrBlock.h"
 
 uint16 _eepromAddr;
 ControlBlock _curBlock;
 
-#define INC_EEPROM_ADDR()   (_eepromAddr += sizeof(_curBlock))
-#define DEC_EEPROM_ADDR()   (_eepromAddr -= sizeof(_curBlock))
+#define BLOCK_SIZE          (sizeof(_curBlock))
+
+#define INC_EEPROM_ADDR()   (_eepromAddr += BLOCK_SIZE)
+#define DEC_EEPROM_ADDR()   (_eepromAddr -= BLOCK_SIZE)
 
 void readCtrBlock() {
-  eeprom_read_block(&_curBlock, (const void*)_eepromAddr, sizeof(_curBlock));
+  eeprom_read_block(&_curBlock, (const void*)_eepromAddr, BLOCK_SIZE);
 }
 
 /**
@@ -38,8 +41,7 @@ ControlBlock *ctrBlockSetup() {
   // check to see if the EEPROM has been initalised
   if (  _curBlock.r   != 0xfa ||
         _curBlock.g != 0xe2 ||
-        _curBlock.b  != 0x11 ||
-        ) return NULL;
+        _curBlock.b  != 0x11 ) return NULL;
   else {
     // read the next block which actually contains
     // control information
@@ -54,11 +56,21 @@ ControlBlock *ctrBlockNext() {
   // $todo implement reversal here
   INC_EEPROM_ADDR();
 
-  if (_eepromAddr >= 512) rewind();
+  if (_eepromAddr > EEPROM_SIZE-BLOCK_SIZE) rewind();
 
   readCtrBlock();
 
   if (_curBlock.duration == 0xff) rewind();
 
   return &_curBlock;
+}
+
+ControlBlock *ctrBlockGoto(uint8 blockNumber) {
+  uint16 newaddr = blockNumber * BLOCK_SIZE;
+  if (newaddr == 0 || newaddr > EEPROM_SIZE-BLOCK_SIZE) return NULL;
+  else {
+    _eepromAddr = newaddr;
+    readCtrBlock();
+    return &_curBlock;
+  }
 }
--- firmware/ctrBlock.h
+++ firmware/ctrBlock.h
@@ -5,4 +5,9 @@
 ControlBlock *ctrBlockSetup();
 ControlBlock *ctrBlockCurrent();
 ControlBlock *ctrBlockNext();
+/**
+ * Return the content of the block at blockNumber, or NULL
+ * if blockNumber is zero or beyond the end of the array
+ */
+ControlBlock *ctrBlockGoto(uint8 blockNumber);
 #endif
